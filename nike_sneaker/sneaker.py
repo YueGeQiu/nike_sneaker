d64a197ce44c66e1ef4635fba6d6c43b15e9e2ac
zhangyan <zhangyan@patsnap.com>
2017-09-14 20:38:35 +0800

    parse release time text

--- nike_sneaker/sneaker.py
+++ nike_sneaker/sneaker.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 
 class NikeNewSneaker(object):
 
@@ -8,8 +10,25 @@ class NikeNewSneaker(object):
         self.img = None
         self.href = None
         self.release_date = None
-        self.release_time = None
+        self._release_time = None
+
+    @property
+    def release_time(self):
+        return self._release_time
+
+    @release_time.setter
+    def release_time(self, raw_text):
+        """ Parse the text from website and convert it to datetime objects """
+        # TODO convert to Datetime objects
+        if not raw_text:
+            return
+        regex_pattern = re.compile('预计(\S*)\s*开始发售')
+        match = regex_pattern.match(raw_text)
+        if match:
+            self._release_time = match.group(1)
+        else:
+            self._release_time = raw_text
 
     def __str__(self):
-        message = "{}\n{}\n{} - {}".format(self._id, self.href, self.release_date, self.release_time)
+        message = "{}\n{}\n{} - {}".format(self._id, self.href, self.release_date, self._release_time)
         return message
